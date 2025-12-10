8a596e07e4a196e4d23816f2bcef3b79d2ff1097
15273 <ryan.mauro@vtsd.com>
2025-12-10 07:58:12 -0500

    Calc

--- calc/calc.js
+++ calc/calc.js
@@ -1,1 +1,12 @@
-//
+// List of buttons
+const buttons = document.querySelectorAll('button');
+const display = document.querySelector('.display')
+
+// Input for each button
+buttons.forEach(
+    function(button){
+        button.addEventListener("click", calculate)
+    }
+)
+
+
