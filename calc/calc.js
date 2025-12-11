005d5cbe5e9f8c9a669662ffaa0ce07ff3299c5c
15273 <ryan.mauro@vtsd.com>
2025-12-11 11:06:49 -0500

    Calc

--- calc/calc.js
+++ calc/calc.js
@@ -5,8 +5,26 @@ const display = document.querySelector('.display')
 // Input for each button
 buttons.forEach(
     function(button){
-        button.addEventListener("click", calculate)
+        button.addEventListener("click", calculate);
     }
 )
 
+// Perform calculation
+function calculate(event){
+    //what was clicked
+    const clickedValue = event.target.value;
 
+    // handle what was clicked
+    // if it's =
+    if(clickedValue === "="){
+        // is display empty?
+        if(display.value !== ""){
+            // calculate the value and display
+            display.value = eval(display.value);
+        }
+    }else if(clickedValue === "C"){
+        display.value ="";
+    }else{
+        display.value += clickedValue;
+    }
+}
